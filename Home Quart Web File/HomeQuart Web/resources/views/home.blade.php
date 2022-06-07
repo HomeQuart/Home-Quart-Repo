23bed6504df3bf54c1dfd7ce72ef3bb4c7543919
France Michael M. Lodonia <93919899+HomeQuart@users.noreply.github.com>
2022-06-07 22:00:56 +0800

    code changes

--- Home Quart Web File/HomeQuart Web/resources/views/home.blade.php
+++ Home Quart Web File/HomeQuart Web/resources/views/home.blade.php
@@ -269,8 +269,34 @@
         
         {{-- message --}}
         {!! Toastr::message() !!}
-        <div class="page-content">
-            <section class="row">
+       
+
+                @foreach ($purok as $puroks)
+                <div class="col-6 col-lg-3 col-md-6">
+                            <div class="card">
+                                <div class="card-body px-3 py-4-5">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="stats-icon green">
+                                                <i class="iconly-boldUser"></i>
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-md-8">
+                                            <h6 class="text-muted font-semibold">PUROK {{ $puroks->purok_name }}</h6>
+                                            <h6 class="font-extrabold mb-0"> Positive Patient: {{ $puroks->positive_counter }}</h6>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                            </tr>
+                </div>
+
+                @endforeach
+
+                <div class="page-content">
+                <section class="row">
                 <div class="col-12 ">
 
                 <div class="col-6 col-lg-3 col-md-6">
@@ -278,8 +304,8 @@
                                 <div class="card-body px-3 py-4-5">
                                     <div class="row">
                                         <div class="col-md-4">
-                                            <div class="stats-icon purple">
-                                                <i class="iconly-boldShow"></i>
+                                            <div class="stats-icon Red">
+                                                <i class="iconly-boldProfile"></i>
                                             </div>
                                         </div>
                                         <div class="col-md-8">
@@ -290,56 +316,6 @@
                                 </div>
                             </div>
                 </div>
-
-                @foreach ($purok as $puroks)
-                <div class="col-6 col-lg-3 col-md-6">
-                            <div class="card">
-                                <div class="card-body px-3 py-4-5">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <div class="stats-icon purple">
-                                                <i class="iconly-boldShow"></i>
-                                            </div>
-                                        </div>
-                                        
-                                        <div class="col-md-8">
-                                            <h6 class="text-muted font-semibold">PUROK</h6>
-                                            <h6 class="font-extrabold mb-0">{{ $puroks->purok_name }}</h6>
-                                            <h6 class="font-extrabold mb-0">{{ $puroks->positive_counter }}</h6>
-                                        </div>
-                                        
-                                    </div>
-                                </div>
-                            </div>
-                </div>
-                @endforeach
-
-              
-                
-                
-                <!-- <div class="card-body px-3 py-4-5">
-                                    {{-- insert the chart for temperature progress here --}}
-                                    <div class="row">
-                                            <div class="col-12">
-                                                <div class="row">
-                                                        
-                                                                    <div class="container">
-                                                                        <h2 style="text-align:center;">TOTAL POSITIVE PATIENT BY PUROK</h2>
-                                                                        <div class="panel panel-primary">
-                                                                           
-                                                                            <div class="panel-body">
-                                                                                <div id="bar-chart"></div>
-                                                                            </div>
-                                                                        </div>
-                                                                        </div>
-                                                                    </div>
-                                                                   
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                </div> -->
 
       
                     {{-- user profile modal --}}
@@ -673,55 +649,6 @@
                             </div>
                         </div>
                     {{-- end user profile modal --}}
-
-                    {{-- <div class="card">
-                        <div class="card-header">
-                            <h4>Recent Messages</h4>
-                        </div>
-                        <div class="card-content pb-4">
-                            <div class="recent-message d-flex px-4 py-3">
-                                <div class="avatar avatar-lg">
-                                    <img src="assets/images/faces/4.jpg">
-                                </div>
-                                <div class="name ms-4">
-                                    <h5 class="mb-1">Hank Schrader</h5>
-                                    <h6 class="text-muted mb-0">@johnducky</h6>
-                                </div>
-                            </div>
-                            <div class="recent-message d-flex px-4 py-3">
-                                <div class="avatar avatar-lg">
-                                    <img src="assets/images/faces/5.jpg">
-                                </div>
-                                <div class="name ms-4">
-                                    <h5 class="mb-1">Dean Winchester</h5>
-                                    <h6 class="text-muted mb-0">@imdean</h6>
-                                </div>
-                            </div>
-                            <div class="recent-message d-flex px-4 py-3">
-                                <div class="avatar avatar-lg">
-                                    <img src="assets/images/faces/1.jpg">
-                                </div>
-                                <div class="name ms-4">
-                                    <h5 class="mb-1">John Dodol</h5>
-                                    <h6 class="text-muted mb-0">@dodoljohn</h6>
-                                </div>
-                            </div>
-                            <div class="px-4">
-                                <button class='btn btn-block btn-xl btn-light-primary font-bold mt-3'>Start
-                                    Conversation</button>
-                            </div>
-                        </div>
-                    </div> --}}
-                    {{-- <div class="card">
-                        <div class="card-header">
-                            <h4>Visitors Profile</h4>
-                        </div>
-                        <div class="card-body">
-                            <div id="chart-visitors-profile"></div>
-                        </div>
-                    </div> --}}
-                </div>
-            </section>
         </div>
         <br><br><br><br><br><br><br><br><br><br><br><br><hr>
         <footer>
--- Home Quart Web File/HomeQuart Web/routes/web.php
+++ Home Quart Web File/HomeQuart Web/routes/web.php
@@ -10,17 +10,6 @@ use App\Http\Controllers\UserManagementController;
 use App\Http\Controllers\LockScreen;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('auth.login');
 });
@@ -143,10 +132,4 @@ Route::get('activity/login/logout', [App\Http\Controllers\UserManagementControll
 Route::get('change/password', [App\Http\Controllers\UserManagementController::class, 'changePasswordView'])->middleware('auth')->name('change/password');
 Route::post('change/password/db', [App\Http\Controllers\UserManagementController::class, 'changePasswordDB'])->name('change/password/db');
 
-// ----------------------------- form staff ------------------------------//
-Route::get('form/staff/new', [App\Http\Controllers\FormController::class, 'index'])->middleware('auth')->name('form/staff/new');
-Route::post('form/save', [App\Http\Controllers\FormController::class, 'saveRecord'])->name('form/save');
-Route::get('form/view/detail', [App\Http\Controllers\FormController::class, 'viewRecord'])->middleware('auth')->name('form/view/detail');
-Route::get('form/view/detail/{id}', [App\Http\Controllers\FormController::class, 'viewDetail'])->middleware('auth');
-Route::post('form/view/update', [App\Http\Controllers\FormController::class, 'viewUpdate'])->name('form/view/update');
-Route::get('delete/{id}', [App\Http\Controllers\FormController::class, 'viewDelete'])->middleware('auth');
+
